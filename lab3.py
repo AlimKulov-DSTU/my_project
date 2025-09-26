ce95a49c644952b422f550d0adf89e4d6ab88f5a
Alim <alimkulov088@mail.ru>
2025-09-26 16:29:55 +0300

    Lab3 Task2

--- lab3.py
+++ lab3.py
@@ -1,3 +1,7 @@
 #Задание 1 список квадратов чисел
 squares = [x**2 for x in range(1, 11)]
 print(squares)
+
+#Задание 2 фильтрация четные числа
+list_filtered = [x for x in range(1, 20) if x % 2 == 0]
+print(list_filtered)
