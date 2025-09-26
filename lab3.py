c5a22aa3a677f877ac4eb9260fcc0f856e21a24f
Alim <alimkulov088@mail.ru>
2025-09-26 16:40:47 +0300

    Lab3 Task3

--- lab3.py
+++ lab3.py
@@ -5,3 +5,8 @@ print(squares)
 #Задание 2 фильтрация четные числа
 list_filtered = [x for x in range(1, 20) if x % 2 == 0]
 print(list_filtered)
+
+#Задание 3 работа со строками 
+words = ['python', 'Java', 'c++', 'Rust', 'go']
+words_new = [word.upper() for word in words if len(word) > 3]
+print(words_new)
